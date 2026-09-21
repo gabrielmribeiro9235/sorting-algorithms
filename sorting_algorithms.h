30ebab4aed7d2753636a13764e2b7040b8fac7fe
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-21 08:06:32 -0300

    feat: create header file

---
+++ sorting_algorithms.h
@@ -0,0 +1,10 @@
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+
+void bubble_sort(int*, int);
+void selection_sort(int*, int);
+void insertion_sort(int*, int);
+void merge_sort(int*, int, int);
+void quick_sort(int*, int, int);
+
+#endif
