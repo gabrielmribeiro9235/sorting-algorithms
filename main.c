71e45b3502474411c440b2e3fe2a0f87d8f5049d
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-21 08:59:50 -0300

    feat: create the print_array helper function

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 #include"src/sorting_algorithms.h"
 
+void print_array(int arr[], int n) {
+    printf("[");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i != n - 1) {
+            printf(", ");
+        }
+    }
+
+    printf("]");
+}
+
 int main() {
 
     return 0;
